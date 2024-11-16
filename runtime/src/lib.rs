acb95e172c0bc471721dca2b33c3d1e54b87835f
Reshetov Denis <reshetovdenis@gmail.com>
2024-11-16 20:49:07 +0100

    Fixes error with weights

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -230,7 +230,7 @@ impl pallet_utility::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type RuntimeCall = RuntimeCall;
 	type PalletsOrigin = OriginCaller;
-	type WeightInfo = weights::pallet_utility::WeightInfo<Runtime>;
+	type WeightInfo = pallet_utility::weights::SubstrateWeight<Runtime>;
 }
 
 impl pallet_timestamp::Config for Runtime {
